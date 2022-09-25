875c8d2e7c83177dd1b8780045c6edd0cde4385f
Sohaib manah <96356071+sohaibMan@users.noreply.github.com>
2022-09-25 19:21:47 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const TIME_BEFORE_LOGOUT=10;
-export const APY_URL='https://it-geeks-de1b9-default-rtdb.europe-west1.firebasedatabase.app//users.json';
+export const APY_URL='https://it-geeks-de1b9-default-rtdb.europe-west1.firebasedatabase.app/users.json';
