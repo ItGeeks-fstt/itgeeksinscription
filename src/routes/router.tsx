610a6a897ff95377228aef5dce0138a86be9b1bf
Sohaib Manah <souhaibemanah@gmail.com>
2023-02-25 23:55:33 +0100

    end of registration

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,23 +1,23 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import React from "react";
-import { FormStep1 } from "../pages/FormStep1/intex";
-import { FormStep2 } from "../pages/FormStep2";
-import { FormStep3 } from "../pages/FormStep3";
-import { FormStep4 } from "../pages/FormStep4";
-import { FormStep5 } from "../pages/FormStep5";
-// import EndOfRegistartion from "../pages/EndOfRegistartion/EndOfRegistartion";
+// import { FormStep1 } from "../pages/FormStep1/intex";
+// import { FormStep2 } from "../pages/FormStep2";
+// import { FormStep3 } from "../pages/FormStep3";
+// import { FormStep4 } from "../pages/FormStep4";
+// import { FormStep5 } from "../pages/FormStep5";
+import EndOfRegistartion from "../pages/EndOfRegistartion/EndOfRegistartion";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         {/* comment this route to disable the end of registration*/}
-        {/* <Route path="/" component={EndOfRegistartion} /> */}
-        <Route path="/" exact component={FormStep1} />
+        <Route path="/" component={EndOfRegistartion} />
+        {/* <Route path="/" exact component={FormStep1} />
         <Route path="/step2" component={FormStep2} />
         <Route path="/step3" component={FormStep3} />
         <Route path="/step4" component={FormStep4} />
-        <Route path="/step5" component={FormStep5} />
+        <Route path="/step5" component={FormStep5} /> */}
       </Switch>
     </BrowserRouter>
   );
